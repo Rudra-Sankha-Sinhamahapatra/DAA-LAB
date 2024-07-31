e03b9b67efa46b87b8da7181b249df6ac2a2d082
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-07-31 10:15:04 +0530

    updated

--- LAB2/p2.c
+++ LAB2/p2.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
 
-int gcd(int a,int b){
-    if(b==0){
+int gcd(int a, int b) {
+    if (b == 0) {
         return a;
-    }
-
-    else {
-        return gcd(b,a%b);
+    } else {
+        return gcd(b, a % b);
     }
 }
 
 int main() {
-    int num1, num2;
+    int n;
 
-    printf("Enter first number: ");
-    scanf("%d", &num1);
-    printf("Enter second number: ");
-    scanf("%d", &num2);
+    printf("Enter an even number of integers: ");
+    scanf("%d", &n);
 
-    printf("GCD of %d and %d is: %d\n", num1, num2, gcd(num1, num2));
+    if (n % 2 != 0) {
+        printf("Please enter an even number of integers.\n");
+        return 1;
+    }
+
+    int numbers[n];
+    printf("Enter %d numbers: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &numbers[i]);
+    }
+
+    for (int i = 0; i < n; i += 2) {
+        int num1 = numbers[i];
+        int num2 = numbers[i + 1];
+        printf("The GCD of %d and %d is %d\n", num1, num2, gcd(num1, num2));
+    }
 
     return 0;
 }
