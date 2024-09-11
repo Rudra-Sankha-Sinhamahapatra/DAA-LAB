c7b74aa1b2361d855550423f076ab6fde2b1f349
rushhaaland <rudrasankhasinhamahapatra@gmail.com>
2024-09-11 09:50:15 +0530

    updating

--- evaluation/LAB7/p1/p1.c
+++ evaluation/LAB7/p1/p1.c
@@ -1,42 +1,34 @@
 #include <stdio.h>
 #include <string.h>
 
-int longestCommonSubstring(char* X, char* Y) {
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int longestCommonSubstring(char X[], char Y[]) {
     int m = strlen(X);
     int n = strlen(Y);
-    int dp[m + 1][n + 1]; 
-    int maxLength = 0; 
 
-  
-    for (int i = 0; i <= m; i++) {
-        for (int j = 0; j <= n; j++) {
-            dp[i][j] = 0;
+    int result = 0;
+
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            int len = 0;
+            while (i + len < m && j + len < n && X[i + len] == Y[j + len])
+                len++;
+            result = max(result, len);
         }
     }
 
-  
-    for (int i = 1; i <= m; i++) {
-        for (int j = 1; j <= n; j++) {
-            if (X[i - 1] == Y[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-                if (dp[i][j] > maxLength) {
-                    maxLength = dp[i][j];
-                }
-            } else {
-                dp[i][j] = 0;
-            }
-        }
-    }
-
-    return maxLength;
+    return result;
 }
 
 int main() {
     char X[] = "KIITFORKIIT";
     char Y[] = "KIITUN";
 
-    int result = longestCommonSubstring(X, Y);
-    printf("Length of Longest Common Substring: %d\n", result);
+    int length = longestCommonSubstring(X, Y);
+    printf("Length of the longest common substring: %d\n", length);
 
     return 0;
 }
