37a8a0aaf3fd8d7c5998eeb7ae304b9d368d850d
rushhaaland <rudrasankhasinhamahapatra@gmail.com>
2024-09-11 09:53:39 +0530

    updating

--- evaluation/LAB7/p1/p1.c
+++ evaluation/LAB7/p1/p1.c
@@ -5,19 +5,32 @@ int max(int a, int b) {
     return (a > b) ? a : b;
 }
 
-int longestCommonSubstring(char X[], char Y[]) {
+int longestCommonSubstring(char X[], char Y[], char resultStr[]) {
     int m = strlen(X);
     int n = strlen(Y);
 
     int result = 0;
+    int endIdx = 0;  
 
     for (int i = 0; i < m; ++i) {
         for (int j = 0; j < n; ++j) {
             int len = 0;
             while (i + len < m && j + len < n && X[i + len] == Y[j + len])
                 len++;
-            result = max(result, len);
+            
+            if (len > result) {
+                result = len;
+                endIdx = i + len - 1; 
+            }
         }
+    }
+
+
+    if (result > 0) {
+        strncpy(resultStr, X + endIdx - result + 1, result);
+        resultStr[result] = '\0';  
+    } else {
+        resultStr[0] = '\0';  
     }
 
     return result;
@@ -26,9 +39,11 @@ int longestCommonSubstring(char X[], char Y[]) {
 int main() {
     char X[] = "KIITFORKIIT";
     char Y[] = "KIITUN";
+    char resultStr[100];  
 
-    int length = longestCommonSubstring(X, Y);
+    int length = longestCommonSubstring(X, Y, resultStr);
     printf("Length of the longest common substring: %d\n", length);
+    printf("Longest common substring: '%s'\n", resultStr);
 
     return 0;
 }
